2d1f18894c1929d41707b408066d4a37702f56ce
yccjs <yccjsalvador@gmail.com>
2022-04-27 14:49:44 +0900

    chore:install dependencies, tested react

---
+++ cc26-linky/src/components/App.jsx
@@ -0,0 +1,10 @@
+// import "./App.css";
+
+export default function App() {
+  return (
+    <>
+      <h1>I think peanut butter is underrated!</h1>
+      <h2>With jelly. Jelly is good.</h2>
+    </>
+  );
+}
