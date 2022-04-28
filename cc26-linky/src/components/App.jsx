10e58bb0b74c9439816645c7272c9634f99843a9
Sakiko Yoshida <sacco221@gmail.com>
2022-04-28 17:08:05 +0900

    feat:add eventhandler for input and fetch data

--- cc26-linky/src/components/App.jsx
+++ cc26-linky/src/components/App.jsx
@@ -8,27 +8,41 @@ import { useEffect, useState} from "react";
 
 
 export default function App() {
-  const [data, setData] = useState(null); // array
+  const [data, setData] = useState(null); 
+  const [upload, setUpload] = useState("");//post data
 
+////fetch data
   const fetchdata = async () => {
     const response = await axios.get("http://localhost:9000/api");  
     const info = response.data;
-    console.log(typeof info.link);
+    // console.log(typeof info.link);
     setData(info);
  };
   
  useEffect(() => {
-　fetchdata();
-  console.log('DATA: ', data)
+  fetchdata();
+  // console.log('DATA: ', data)
  }
  , []);
- 
+
+
+///post data
+
+
+//  useEffect(() => {
+//   //post function
+//   
+//   },[upload]);
+
+///tag filter
+
 
   return (
       <div className="App">
         <Navbar />
-        <Input className="input-container" />
+        <Input className="input-container" setUpload={setUpload} upload = {upload} />
         <Display content={data}/>
+        
       </div>                            
   );
 }
