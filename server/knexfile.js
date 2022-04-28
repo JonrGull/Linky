1c5cb4b66b7ac1c26c1bfa437c6536b846f70672
Thomas Ryan <DWTrip@gmail.com>
2022-04-28 17:40:47 +0900

    Heroku: Different knex file config

--- server/knexfile.js
+++ server/knexfile.js
@@ -32,18 +32,27 @@ require("dotenv").config({
 // },
 
 module.exports = {
+  // client: "pg",
+  // connection: process.env.DB_URL || {
+  //   host: process.env.DB_HOST || "127.0.0.1",
+  //   port: process.env.DB_PORT || 9000,
+  //   database: process.env.DB_NAME,
+  //   user: process.env.DB_USER,
+  //   password: process.env.DB_PASSWORD,
+  // },
+  // migrations: {
+  //   directory: "./db/migrations",
+  // },
+  // seeds: {
+  //   directory: "./db/seeds",
+  // },
   client: "pg",
-  connection: process.env.DB_URL || {
-    host: process.env.DB_HOST || "127.0.0.1",
-    port: process.env.DB_PORT || 9000,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-  },
+  connection: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
   migrations: {
-    directory: "./db/migrations",
+    directory: "./data/migrations",
   },
   seeds: {
-    directory: "./db/seeds",
+    directory: "./data/seeds",
   },
 };
