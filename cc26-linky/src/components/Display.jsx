31aa47f04d5708413693c2babf8e81cfa907d683
Sakiko Yoshida <sacco221@gmail.com>
2022-04-29 15:47:33 +0900

    feat: post function and css works

--- cc26-linky/src/components/Display.jsx
+++ cc26-linky/src/components/Display.jsx
@@ -1,31 +1,31 @@
 import React from "react";
 
 export default function Display({content}) {
-// console.log("content in Display",content);
   if (content !== null) {
-    const contentLink = content[0].link;
-    // console.log(typeof contentLink)
-    // console.log(contentLink)
+    
   return (
     <div className="display-container" >
       <p className="displayMessage">Find your favorites</p>
         {content.map((item, index) => {
+          let strTags = "";
           const itemUrl = item.link;
-          const itemTags = JSON.stringify(item.tags); // will be changed later!
-          return (
-            <div className="item-container" key={index} >
-              <a href={contentLink}>Click the link!</a>
-              <p>{item.description}</p>
-            </div>
+          for(let i=0; i < item.tags.length ; i++){
+            strTags += "#" + item.tags[i] + "  ";
+          }
+        return (
+          <div className="item-container" key={index} >
+           <a href={itemUrl}>Click the link!</a>
+           <p className="item-desc">{item.description}</p>
+           <p className="item-tags">{strTags}</p>
+          </div>
           )
         })
-
         }
-        {/* <h1>Test</h1> */}
-        {/* <a href={contentLink}>This is a youtube link</a> */}
     </div> 
   );
 } else {
-  return <div> "loading"...</div>
-}
+    return (
+    <div> Loading....</div>
+   )
+ }
 }
