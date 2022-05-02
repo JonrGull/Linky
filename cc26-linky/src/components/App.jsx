45de02e5e3a64e87e1aeed4177d221971bdbfe63
Dominic Lowes <domin@Dominics-MacBook-Air.local>
2022-05-02 14:31:48 +0900

    Feat: add login component

--- cc26-linky/src/components/App.jsx
+++ cc26-linky/src/components/App.jsx
@@ -8,12 +8,21 @@ import { useEffect, useState } from "react";
 
 export default function App() {
   const [data, setData] = useState(null);
+  const [errorMessage, setErrorMessage] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [userData, setUserData] = useState([]);
 
   const fetchdata = async () => {
     const response = await axios.get("https://cc26-linky.herokuapp.com/api");
     const info = response.data;
     setData(info);
   };
+
+  const fetchUser = async () => {
+    const response = await axios.get("/user");
+    const userInfo = response.data;
+    setUserData(userInfo);
+  }
 
   const postData = async (data) => {
     await axios.post("https://cc26-linky.herokuapp.com/newpost", data);
