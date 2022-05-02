c4dca1d529acc1687cf0c78df2d194dc83575294
JonRGull <25947147+JonrGull@users.noreply.github.com>
2022-05-02 12:24:23 +0900

    Add:change css to better reflect our company values

--- cc26-linky/src/components/Display.jsx
+++ cc26-linky/src/components/Display.jsx
@@ -5,12 +5,12 @@ export default function Display({ content }) {
   if (content !== null) {
     return (
       <div className="display-container">
-        <p className="displayMessage">Find your favorites</p>
+        <p className="displayMessage">Find your favourites</p>
         {content.map((item, index) => {
           const itemUrl = item.link;
           return (
             <div className="item-container" key={index}>
-              <a href={itemUrl}>Click the link!</a>
+              <a href={itemUrl}>Don't lick the link!</a>
               <p className="item-desc">{item.description}</p>
             </div>
           );
